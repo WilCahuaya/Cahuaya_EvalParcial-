ea85948a345c8680fd80ef6b46341dad0e96cf62
wilCahuaya <48217068@continental.edu.pe>
2022-10-12 02:17:23 -0500

    listar curso completo

--- app/src/main/java/com/example/cahuaya_evalparcial/Entidades/CursoAdapter.java
+++ app/src/main/java/com/example/cahuaya_evalparcial/Entidades/CursoAdapter.java
@@ -1,7 +1,63 @@
 package com.example.cahuaya_evalparcial.Entidades;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate;
 
-public class CursoAdapter  {
+import com.example.cahuaya_evalparcial.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class CursoAdapter extends RecyclerView.Adapter<CursoAdapter.CursoHolder> {
+    List<Curso> listaCurso;
+
+    public CursoAdapter(List<Curso> listaCurso){
+        this.listaCurso=listaCurso;
+    }
+
+    @NonNull
+    @Override
+    public CursoHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View vista= LayoutInflater.from(parent.getContext()).inflate(R.layout.curso_imagen_lista,parent,false);
+        RecyclerView.LayoutParams layoutParams=new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        vista.setLayoutParams(layoutParams);
+        return new CursoHolder(vista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NotNull CursoAdapter.CursoHolder holder, int position) {
+        holder.txtNombre.setText(String.valueOf(listaCurso.get(position).getNombre()));
+        holder.txtDescripcion.setText(String.valueOf(listaCurso.get(position).getDescripcion()));
+        holder.txtNivel.setText(String.valueOf(listaCurso.get(position).getNivel()));
+        if (listaCurso.get(position).getImagen() !=null)
+            holder.imagen.setImageBitmap(listaCurso.get(position).getImagen());
+        else
+            holder.imagen.setImageResource(R.drawable.ic_launcher_background);
+
+    }
+    @Override
+    public int getItemCount() {
+        return listaCurso.size();
+    }
+
+    public class CursoHolder extends RecyclerView.ViewHolder{
+        TextView  txtNombre,txtDescripcion,txtNivel;
+        ImageView imagen;
+
+        public CursoHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNombre=itemView.findViewById(R.id.txtNameViewCurso);
+            txtDescripcion=itemView.findViewById(R.id.txtDescripcionViewCurso);
+            txtNivel=itemView.findViewById(R.id.txtNivelLC);
+            imagen=itemView.findViewById(R.id.imgIcon);
+        }
+    }
 }
--- app/src/main/java/com/example/cahuaya_evalparcial/ui/ListarCurso/ListarCursoFragment.java
+++ app/src/main/java/com/example/cahuaya_evalparcial/ui/ListarCurso/ListarCursoFragment.java
@@ -1,18 +1,40 @@
 package com.example.cahuaya_evalparcial.ui.ListarCurso;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.cahuaya_evalparcial.Entidades.Curso;
+import com.example.cahuaya_evalparcial.Entidades.CursoAdapter;
 import com.example.cahuaya_evalparcial.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class ListarCursoFragment extends Fragment {
+import java.util.ArrayList;
 
+
+public class ListarCursoFragment extends Fragment implements Response.Listener<JSONObject>, Response.ErrorListener{
+    RecyclerView recyclerViewCurso;
+    ArrayList<Curso> listaCurso;
+    ProgressDialog dialog;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +46,57 @@ public class ListarCursoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista=inflater.inflate(R.layout.fragment_listar_curso, container, false);
+        listaCurso= new ArrayList<>();
+        recyclerViewCurso=(RecyclerView) vista.findViewById(R.id.idRecyclerListaCurso);
+        recyclerViewCurso.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerViewCurso.setHasFixedSize(true);
+        request = Volley.newRequestQueue(getContext());
 
+        //llamarWebservice
+        listarCurso();
         return vista;
+    }
+
+    private void listarCurso() {
+        dialog =new ProgressDialog(getContext());
+        dialog.setMessage("Consultando Personajes");
+        dialog.show();
+        String url="http://192.168.19.91:8080/apis/listarcurso.php";
+        jsonObjectRequest= new JsonObjectRequest (Request.Method.GET, url, null,this,this);
+        request.add(jsonObjectRequest);
+    }
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+        Toast.makeText(getContext(), "aqui"+error, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+        Curso curso=null;
+        JSONArray json=response.optJSONArray("curso");
+        try {
+            for(int i=0; i<json.length();i++) {
+                curso=new Curso();
+                JSONObject jsonObject=null;
+                jsonObject=json.getJSONObject(i);
+                curso.setNombre(jsonObject.optString("nombre"));
+                curso.setDescripcion(jsonObject.optString("descripcion"));
+                curso.setNivel(jsonObject.optString("nivel"));
+                curso.setDato(jsonObject.optString("imagen"));
+                listaCurso.add(curso);
+            }
+            dialog.hide();
+            CursoAdapter adapter=new CursoAdapter(listaCurso);
+            recyclerViewCurso.setAdapter(adapter);
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+            Toast.makeText(getContext(), "No hay conexion con el servidor", Toast.LENGTH_SHORT).show();
+            dialog.hide();
+        }
+
+
     }
 }
